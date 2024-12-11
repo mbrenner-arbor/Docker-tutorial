c298911c751e967cf51212b8b50d27a4f808ce10
mbrenner-arbor <mbrenner@arbor.daicel.com>
2024-12-11 10:27:54 -0500

    Initial Commit

--- docker_tutorial.py
+++ docker_tutorial.py
@@ -1,13 +1,16 @@
 import argparse
-
+import numpy as np
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="docker_tutorial.py",
-                                     description="docker tutorial, prints input file contents.",
+                                     description="docker tutorial, prints input file contents and numpy array.",
                                      usage="python3 docker_tutorial.py [file]")
     parser.add_argument("file", help="file to print")
     args = parser.parse_args()
     file = args.file
 
+    array = np.zeros((10, 10))
+
     with open(file, 'r') as f:
         print(f.read())
+    print(array)
